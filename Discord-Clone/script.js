a3de3c5205dec403f0793d7957bdb56e0437ac88
shiwang aryan <126906676+arshiwang@users.noreply.github.com>
2023-11-28 10:37:22 +0530

    HTML - CSS - JS  |  Scrolling animation
    Fade in animation on scrolling , now on scrolling the text and images appears smoothly with animation which was done by using vanilla JS and css only

--- Discord-Clone/script.js
+++ Discord-Clone/script.js
@@ -1,9 +1,9 @@
 var sidemenu = document.getElementById("slidemenu");
-var blueDiv= document.getElementById('blue-div');
-var createInviteRow= document.getElementById('create-an-invite');
+var blueDiv = document.getElementById("blue-div");
+var createInviteRow = document.getElementById("create-an-invite");
 function openSlideMenu() {
   sidemenu.style.transform = "translateX(0)";
-  blueDiv.style.filter= "brightness(70%)";
+  blueDiv.style.filter = "brightness(70%)";
   createInviteRow.style.filter = "brightness(70%)";
 }
 
@@ -20,3 +20,19 @@ function menuClick(x) {
   menuList.forEach((item) => item.classList.remove("active"));
   menuElement.classList.add("active");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  var rows = document.querySelectorAll(".scrolling");
+  var i = 0;
+  function animationFadeIn() {
+    var winHeight = screen.height;
+    var elementTop = rows[i].getBoundingClientRect().top;
+
+    if (elementTop < winHeight * 0.7) {
+      rows[i].classList.add("animateFade");
+      i++;
+    }
+  }
+  window.addEventListener("scroll", animationFadeIn);
+  animationFadeIn();
+});
